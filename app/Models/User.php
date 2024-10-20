42314c42bd630eb87f95244c97b21b6a2a53f4dc
janickkiy <janickiy@mail.ru>
2024-10-20 15:48:42 +0300

    bugfix

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        'login',
+        'description',
+        'role',
         'password',
     ];
 
